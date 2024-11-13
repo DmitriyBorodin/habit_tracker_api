d63adf3ff2dabf61b3564f97a7520d0f21fddb47
Dmitriy Borodin <dmitriyborodinpm@gmail.com>
2024-11-13 17:04:49 +0300

    updated urls

---
+++ habit/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from habit.apps import HabitConfig
+from habit.views import HabitCreateAPIView, HabitListAPIView, HabitRetrieveAPIView, HabitDestroyAPIView, \
+    HabitUpdateAPIView, PublicHabitListAPIView
+
+
+app_name = HabitConfig.name
+
+urlpatterns = [
+    path('habits/', HabitListAPIView.as_view(), name='habit-list'),
+    path('habits/create/', HabitCreateAPIView.as_view(), name='habit-create'),
+    path('habits/<int:pk>/', HabitRetrieveAPIView.as_view(), name='habit-retrieve'),
+    path('habits/<int:pk>/update/', HabitUpdateAPIView.as_view(), name='habit-update'),
+    path('habits/<int:pk>/delete/', HabitDestroyAPIView.as_view(), name='habit-delete'),
+    path('habits/public/', PublicHabitListAPIView.as_view(),name='habit-list-public'),
+]
