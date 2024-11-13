bd7f6e75b73d2f486f5a3cfef27c64d8ac5e1837
Dmitriy Borodin <dmitriyborodinpm@gmail.com>
2024-11-13 14:59:14 +0300

    created user and habit models, made migrations

--- habit/models.py
+++ habit/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from tkinter.constants import CASCADE
 
-# Create your models here.
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+from django_celery_beat.validators import minute_validator
+
+from config import settings
+
+NULLABLE = {'blank': True, 'null': True}
+
+class Habit(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, **NULLABLE, verbose_name='Создатель привычки')
+    place = models.CharField(max_length=255, verbose_name='Место')
+    time = models.TimeField(verbose_name='Время')
+    action = models.CharField(max_length=255, verbose_name='Действие')
+    is_pleasant = models.BooleanField(default=False, verbose_name='Признак приятной привычки')
+    related_habit = models.ForeignKey('self', related_name='retalted_to', on_delete=models.SET_NULL, **NULLABLE, verbose_name='Связанная привычка')
+    periodicity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(7)], verbose_name='Периодичность')
+    reward = models.CharField(max_length=255, **NULLABLE, verbose_name='Награда')
+    duration = models.DurationField(verbose_name='Время на выполнение')
+    is_public = models.BooleanField(default=False, verbose_name='Признак публичной привычки')
+
+    class Meta:
+        verbose_name = 'Привычка',
+        verbose_name_plural = 'Привычки'
+
+    def __str__(self):
+        return f'Привычка пользователя {self.owner} - я буду {self.action} в {self.time} в {self.place}'
